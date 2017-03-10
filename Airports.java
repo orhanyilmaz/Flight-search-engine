632cea54f1ab122c2b8a1832858326413eb45e1e
Orhan YILMAZ <orhan__yilmazz@hotmail.com>
2017-03-10 18:58:46 +0200

    Create Airports.java

---
+++ Airports.java
@@ -0,0 +1,11 @@
+
+public class Airports {
+	String cityName;
+	String[] airport = new String[15];
+	int[] airportIndex = new int[15];
+
+	Airports(){
+		for(int i=0 ; i<airport.length ; i++)
+			airport[i]="İ";
+	}
+}
