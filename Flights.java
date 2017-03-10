23bb0df13ee7f132fb6474ffbff2932991d9943b
Orhan YILMAZ <orhan__yilmazz@hotmail.com>
2017-03-10 18:59:30 +0200

    Create Flights.java

---
+++ Flights.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+import java.util.Calendar;
+
+public class Flights {
+	String flightId;
+	String start;
+	String end;
+	Calendar dept_date = Calendar.getInstance();
+	int durationHour;
+	int durationMinute;
+	Date flightEnd;
+	int price=0;	
+}
